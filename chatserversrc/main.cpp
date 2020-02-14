0222ba844b1106fbb0111f95d08312e56717ae82
blackhidden <13516355198@163.com>
2020-02-14 11:42:21 +0800

    fix bugs

--- chatserversrc/main.cpp
+++ chatserversrc/main.cpp
@@ -15,6 +15,10 @@
 #include "../net/EventLoopThreadPool.h"
 #include "../mysqlmgr/MysqlManager.h"
 
+#ifndef WIN32
+#include <string.h>
+#include "../utils/DaemonRun.h"
+#endif 
 
 #include "UserManager.h"
 #include "ChatServer.h"
@@ -23,10 +27,53 @@
 
 using namespace net;
 
+#ifdef WIN32
+//初始化Windows socket库
+NetworkInitializer windowsNetworkInitializer;
+#endif
+
 EventLoop g_mainLoop;
+
+#ifndef WIN32
+void prog_exit(int signo)
+{
+    std::cout << "program recv signal [" << signo << "] to exit." << std::endl;
+
+    Singleton<MonitorServer>::Instance().uninit();
+    Singleton<HttpServer>::Instance().uninit();
+    Singleton<ChatServer>::Instance().uninit();
+    g_mainLoop.quit();
+
+    CAsyncLog::uninit();
+}
+#endif
 
 int main(int argc, char* argv[])
 {
+#ifndef WIN32
+    //设置信号处理
+    signal(SIGCHLD, SIG_DFL);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, prog_exit);
+    signal(SIGTERM, prog_exit);
+
+
+    int ch;
+    bool bdaemon = false;
+    while ((ch = getopt(argc, argv, "d")) != -1)
+    {
+        switch (ch)
+        {
+        case 'd':
+            bdaemon = true;
+            break;
+        }
+    }
+
+    if (bdaemon)
+        daemon_run();
+#endif
+    
     CConfigFileReader config("etc/chatserver.conf");
 
     const char* logbinarypackage = config.getConfigName("logbinarypackage");
@@ -40,6 +87,29 @@ int main(int argc, char* argv[])
     }
    
     std::string logFileFullPath;
+
+#ifndef WIN32
+    const char* logfilepath = config.getConfigName("logfiledir");
+    if (logfilepath == NULL)
+    {
+        LOGF("logdir is not set in config file");
+        return 1;
+    }
+
+    //如果log目录不存在则创建之
+    DIR* dp = opendir(logfilepath);
+    if (dp == NULL)
+    {
+        if (mkdir(logfilepath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0)
+        {
+            LOGF("create base dir error, %s , errno: %d, %s", logfilepath, errno, strerror(errno));
+            return 1;
+        }
+    }
+    closedir(dp);
+
+    logFileFullPath = logfilepath;
+#endif
 
     const char* logfilename = config.getConfigName("logfilename");
     logFileFullPath += logfilename;
